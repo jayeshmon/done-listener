944083f82432e68d50a7e51767e1c24947dc0aec
jayeshs.transight <jayeshs.transight@gmail.com>
2024-08-05 23:53:30 +0530

    changed

--- models/DroneData.mjs
+++ models/DroneData.mjs
@@ -38,40 +38,5 @@ const DroneDataSchema = new mongoose.Schema({
   FN: String
 }, { collection: 'drone_data' });
 
-    const droneDataSchema = new mongoose.Schema({
-      t: String,
-      VD: String,
-      FV: String,
-      AD: Number,
-      PS: String,
-      v: String,
-      DM: String,
-      T: String,
-      l: String,
-      g: String,
-      s: String,
-      ALT: String,
-      SN: String,
-      HD: String,
-      FL_MOD: String,
-      MC: String,
-      MN: String,
-      MV: String,
-      IV: String,
-      ROLL: String,
-      YAW: String,
-      PITCH: String,
-      WTR_QTY: String,
-      CONLQD: String,
-      FLW_RT: String,
-      GPSCNT: String,
-      TNKLVL: String,
-      PLAN_AREA: String,
-      COV_AREA: String,
-      BOUNDARY: String,
-      SS: String,
-      p: Number,
-      FN: String
-    }, { collection: 'drone_data' });
-    const DroneData = mongoose.model('DroneData', droneDataSchema);
+    
 export { DroneData };
