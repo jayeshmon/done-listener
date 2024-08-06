11b4db84c2a486ae883bc3fe7b72ba1f9d1c4f38
jayeshs.transight <jayeshs.transight@gmail.com>
2024-08-07 00:46:59 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const { createClient } = require('redis');
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Enable CORS for all origins
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+
+  // If the request method is OPTIONS, return a 200 status code
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
